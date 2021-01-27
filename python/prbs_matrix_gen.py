93a8e4aadf7780b054354e3e662083ec14005710
Changsong Li <poena@163.com>
2021-01-27 16:47:26 +0800

    add parameter

--- python/prbs_matrix_gen.py
+++ python/prbs_matrix_gen.py
@@ -5,14 +5,14 @@
 # Date              : 02.12.2020
 # Last Modified Date: 02.12.2020
 # Last Modified By  : Changsong Li <poena@163.com>
-
+import sys, getopt
 import numpy as np
 
 pseudo_predefine_dict = {'prbs_7': [0xff, [7, 6]],
                          'prbs_9': [0x1f5, [9, 5]],
                          'prbs_15': [0x7d6e, [15, 14]],
-                         'prbs_23': ['0x7d6e5d', [23, 18]],
-                         'prbs_31': ['0x7ffae000', [31, 28]]}
+                         'prbs_23': [0x7d6e5d, [23, 18]],
+                         'prbs_31': [0x7ffae000, [31, 28]]}
 
 def bin2hex(bin_list,out_len):
     total_len = len(bin_list)
@@ -63,6 +63,12 @@ def base_matrix_gen(prbs_type,parallel_width):
 
     return MM[0:parallel_width,:]
 
+def get_base_matrix(prbs_mode,parallel_width):
+    prbs_expr = pseudo_predefine_dict[prbs_mode]
+    prbs_type = prbs_expr[1]
+    M = base_matrix_gen(prbs_type,parallel_width)
+    return M
+
 def prbs_next(curr_state,prbs_mode,parallel_width):
     prbs_expr = pseudo_predefine_dict[prbs_mode]
     prbs_len = prbs_expr[1][0]
@@ -80,14 +86,44 @@ def prbs_next(curr_state,prbs_mode,parallel_width):
     print(result_hex)
     return result_hex
 
-
-if __name__ == '__main__':
-    #base_matrix_gen(pseudo_predefine_dict['prbs_7'])
-    #base_matrix_gen(pseudo_predefine_dict['prbs_23'])
-    #curr_state = 0x7f
-    curr_state = 0x7fffffff
+def prbs_gen(prbs_mode,prbs_len,init_value,prbs_width):
+    if(init_value == None):
+        curr_state = pseudo_predefine_dict[prbs_mode][0]
+    else:
+        curr_state = init_value
+    print(curr_state)
     for l in range(1):
-        next_state = prbs_next(curr_state,'prbs_31',80)
+        next_state = prbs_next(curr_state,prbs_mode,prbs_width)
         curr_state = int(next_state[-1],16)&0x7fffffff
     #prbs_next(0xff,'prbs_7')
 
+def cmd_help():
+    print("prbs_matrix_gen.py --mode='prbs_7' --length=80 --width=32")
+
+def main(argv):
+    prbs_mode = 'prbs_7'
+    prbs_len = 160
+    prbs_width = 32
+    prbs_seed = None
+    try:
+        opts, args = getopt.getopt(argv,"hl:m:w:s:",["length=","mode=","width=","seed="])
+    except getopt.GetoptError:
+        cmd_help()
+    for opt, arg in opts:
+        if opt == '-h':
+            cmd_help()
+            sys.exit()
+        elif opt in ("-l","--length"):
+            prbs_len = int(arg)
+        elif opt in ("-m","--mode"):
+            prbs_mode = arg
+        elif opt in ("-w","--width"):
+            prbs_width = int(arg)
+        elif opt in ("-s","--seed"):
+            prbs_seed = int(arg,16)
+
+    print([prbs_mode,prbs_len,prbs_width,prbs_seed])
+    prbs_gen(prbs_mode,prbs_len,prbs_seed,prbs_width)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
